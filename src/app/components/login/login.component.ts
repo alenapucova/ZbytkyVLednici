39e27554f516da8904c07b4ccbb8c9a977ab5c05
Alena P <alena.pucova@centrum.cz>
2020-07-30 11:08:31 +0200

    backend set up

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, Inject, OnInit } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 export interface DialogData {
   animal: string;
@@ -7,23 +7,18 @@ export interface DialogData {
 }
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-
- 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
   constructor(
     public dialogRef: MatDialogRef<LoginComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
-
 }
-
---
+++ src/app/user.model.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: String;
+  firstName: String;
+  lastName: String;
+  email: String;
+  password: String;
+}
