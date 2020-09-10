c21d1bab36b38c3dba1a5e2e4091f79b3aad6259
Alena P <alena.pucova@centrum.cz>
2020-09-10 20:26:22 +0200

    portions resolve

--- src/app/models/criteria.model.ts
+++ src/app/models/criteria.model.ts
@@ -1,9 +1,10 @@
 import { NameAndValue } from './name-and-value.model';
+import { PortionsService } from '../portions.service';
 
 export class Criteria {
     time: number;
-    portions: number;
+    portions: number = PortionsService.DEFAULT_PORTIONS;
     style: NameAndValue[];
     difficulty: NameAndValue[];
-    typeOfMeal:NameAndValue[];
+    typeOfMeal: NameAndValue[];
 }
